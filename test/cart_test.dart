05d733b9d8a778d0b4f73ad62b7698dd5998be8e
TetsuFe <tetsubeg1ner27@gmail.com>
2020-11-23 15:41:01 +0900

    Add value notifier test

--- test/cart_test.dart
+++ test/cart_test.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:value_notifier_sample/cart.dart';
+import 'package:value_notifier_sample/cart_notifier.dart';
+import 'package:value_notifier_sample/cart_repository.dart';
 import 'package:value_notifier_sample/item.dart';
 
 void main() {
@@ -7,5 +9,16 @@ void main() {
     final cart = Cart([Item('grape', 300), Item('orange', 100)]);
     expect(cart.totalPrice, 400);
     expect(cart.totalPriceWithUnit, '400円');
+  });
+
+  test('cart value notifier', () async {
+    final cartNotifier = CartNotifier(repository: CartRepository());
+    final cart = Cart([Item('grape', 300), Item('orange', 100)]);
+    Cart lastValue;
+    cartNotifier.addListener(() {
+      lastValue = cartNotifier.value;
+    });
+    cartNotifier.value = cart;
+    expect(lastValue, cart);
   });
 }
