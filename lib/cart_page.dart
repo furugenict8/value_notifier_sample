d854ce0878e09a32bb1f09ef65caee7ffd275e85
TetsuFe <tetsubeg1ner27@gmail.com>
2020-11-23 15:05:58 +0900

    Extract CartView widget

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -5,35 +5,42 @@ import 'cart_notifier.dart';
 import 'cart_repository.dart';
 
 class CartPage extends StatelessWidget {
-  final cartNotifier = CartNotifier(repository: CartRepository());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('カート'),
       ),
-      body: ValueListenableBuilder<Cart>(
-        valueListenable: cartNotifier,
-        builder: (context, cart, _) {
-          if (cart == null) {
-            return const CircularProgressIndicator();
-          }
-          return Center(
-            child: Column(
-              children: [
-                ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: cart.items.length,
-                  itemBuilder: (context, index) => ListTile(
-                      title: Text('${cart.items[index].name}'),
-                      subtitle: Text('${cart.items[index].priceWithUnit}')),
-                ),
-                Text('合計金額: ${cart.totalPriceWithUnit}'),
-              ],
-            ),
-          );
-        },
-      ),
+      body: CartView(),
+    );
+  }
+}
+
+class CartView extends StatelessWidget {
+  final cartNotifier = CartNotifier(repository: CartRepository());
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<Cart>(
+      valueListenable: cartNotifier,
+      builder: (context, cart, _) {
+        if (cart == null) {
+          return const CircularProgressIndicator();
+        }
+        return Center(
+          child: Column(
+            children: [
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: cart.items.length,
+                itemBuilder: (context, index) => ListTile(
+                    title: Text('${cart.items[index].name}'),
+                    subtitle: Text('${cart.items[index].priceWithUnit}')),
+              ),
+              Text('合計金額: ${cart.totalPriceWithUnit}'),
+            ],
+          ),
+        );
+      },
     );
   }
 }
