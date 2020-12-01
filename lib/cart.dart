4336e42506ab7b790ca78a22870b3ec28f5c92f4
TetsuFe <tetsubeg1ner27@gmail.com>
2020-12-01 22:54:16 +0900

    Delete unused property

--- lib/cart.dart
+++ lib/cart.dart
@@ -9,9 +9,7 @@ class Cart {
   final List<Item> items;
   int get totalPrice =>
       items.length > 0 ? items.map((a) => a.price).reduce((a, b) => a + b) : 0;
-
   String get totalPriceWithUnit => '$totalPrice円';
-  int get itemsCount => items.length;
 
   Cart remove(Item item) {
     return Cart(
