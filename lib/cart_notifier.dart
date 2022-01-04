3bcdd103c3337feeddc05d9a459ffef3ebc29382
Ryouhei Furugen <furugenict8@gmail.com>
2022-01-05 07:58:46 +0900

    #1 CartNotifier()についてコメント追加

--- lib/cart_notifier.dart
+++ lib/cart_notifier.dart
@@ -4,11 +4,17 @@ import 'cart.dart';
 import 'cart_repository.dart';
 import 'item.dart';
 
+
 class CartNotifier extends ValueNotifier<Cart> {
+  // CartNotifierのコンストラクタ。空のコンストラクタを作っていると思われる。
+  //　CartRepositoryを引数にとる→super(ValueNotifier)のコンストラクタを呼び出し、そこでinit()している。
+  // _valueをnullにしているので、空のカートを作っているイメージだと思う。
   CartNotifier({@required this.repository}) : super(null) {
     init();
   }
 
+  // なぜここでasync awaitを使っているのか？
+  // →おそらく、repository.get()がasyncを使っているからかな。
   void init() async {
     // repository.get()からはCartのインスタンスが取得できる
     value = await repository.get();
