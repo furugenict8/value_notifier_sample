d5f1f707d3ec37731a2b75e7db958eb8845e1ab2
Ryouhei Furugen <furugenict8@gmail.com>
2022-01-05 18:33:19 +0900

    #1 CartNotifierのremove()についてコメント追加

--- lib/cart_notifier.dart
+++ lib/cart_notifier.dart
@@ -5,6 +5,7 @@ import 'cart_repository.dart';
 import 'item.dart';
 
 
+// Cartの状態管理をするValueNotifier
 class CartNotifier extends ValueNotifier<Cart> {
   // CartNotifierのコンストラクタ。空のコンストラクタを作っていると思われる。
   //　CartRepositoryを引数にとる→super(ValueNotifier)のコンストラクタを呼び出し、そこでinit()している。
@@ -15,11 +16,18 @@ class CartNotifier extends ValueNotifier<Cart> {
 
   // なぜここでasync awaitを使っているのか？
   // →おそらく、repository.get()がasyncを使っているからかな。
+  // valueはValueNotifierで定義されているfield。Genericsで指定されているインスタンスが入る。
+  // 今回はCart
+  // つまり、以下ではValueNotifierで監視しているCartを初期化している。
   void init() async {
     // repository.get()からはCartのインスタンスが取得できる
     value = await repository.get();
   }
 
+  // valueはValueNotifierのvalueであり、Cartが入っている。
+  // Cartのremove()をして、valueに再代入。
+  // Cartのremove()は、
+  // List itemsから引数のitemを削除し、List itemsを作り直す。
   void remove(Item item) {
     this.value = value.remove(item);
   }
