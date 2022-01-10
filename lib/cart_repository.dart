ee6498594a44595caa563964fba83d3a425b301d
Ryouhei Furugen <furugenict8@gmail.com>
2022-01-10 18:16:47 +0900

    #1 CartRepositoryにコメント追加

--- lib/cart_repository.dart
+++ lib/cart_repository.dart
@@ -2,7 +2,9 @@ import 'cart.dart';
 import 'item.dart';
 
 class CartRepository {
+  //　非同期関数にしているのは、通常はデータベースとかから持ってくるデータがあるからだと思う。
   Future<Cart> get() async {
+    //　今回はデータベースからではなく、実際にデータ直接入れているので、Future.value（）を使っている。
     return Future.value(Cart([Item(1, 'apple', 200), Item(2, 'banana', 100)]));
   }
 }
